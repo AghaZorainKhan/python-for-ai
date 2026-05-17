23f45ebcd6f2d71dd15395a117d968666184c9ff
Agha Zorain Khan <agha_zorain@yahoo.com>
2026-05-17 19:09:04 +0500

    variables

--- hello.py
+++ hello.py
@@ -1,2 +1,16 @@
-print("Hello World!")
-print("its a nice day to learn python")
+#variables
+name = "Alice"
+age = 30
+print(f"Name: {name}, Age: {age}")
+
+#snake_case
+first_name = "Alice"
+last_name = "Smith"
+age = 30
+print(f"First Name: {first_name}, Last Name: {last_name}, Age: {age}")
+
+#changing variable values
+age1 = 31
+print(age1)# age1 will print 31
+age1 = 32
+print(age1)# age1 will print 32
