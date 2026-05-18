2390e2833e3641bb1d4e809122d745ac70d7fb21
Agha Zorain Khan <agha_zorain@yahoo.com>
2026-05-19 03:33:02 +0500

    Data Types

---
+++ datatypes.py
@@ -0,0 +1,68 @@
+#strings 
+
+first_name = " Agha Zorain"
+last_name = "Khan"
+
+full_name = first_name + " " + last_name
+print(full_name*2)
+
+long_dash = "-" * 20
+print(long_dash)
+
+len(full_name)
+
+#booleans
+
+is_logged_in = True
+is_admin = False
+
+print(is_logged_in)
+print(is_admin)
+
+age = 25
+can_vote = age >= 18
+print(can_vote)
+
+#numbers
+
+total = 10 + 5
+print(total)
+
+power = 2 ** 3
+print(power)
+
+#comparision operators
+
+age = 18
+
+print(age == 18)    
+print(age != 21)    
+print(age > 17)     
+print(age < 20)    
+print(age >= 18)    
+print(age <= 18)   
+
+#arthematic operators
+
+# Basic math
+print(10 + 3)   
+print(10 - 3)   
+print(10 * 3)   
+print(10 / 3)  
+
+# Special operators
+print(10 // 3)  
+print(10 % 3)   
+print(10 ** 3)  
+
+#logical operators (and, or, not)
+
+age = 27
+has_license = True 
+drunk = True
+
+can_drive = age >= 18 and has_license and not drunk
+print(can_drive)
+
+
+
